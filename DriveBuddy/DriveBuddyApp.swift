7606e5a8471c08efb0a8339c81b699fb03b8ae32
Katherine Palevich <katherine.palevich@gmail.com>
2021-03-09 10:36:12 -0800

    Initial Commit

---
+++ DriveBuddy/DriveBuddyApp.swift
@@ -0,0 +1,20 @@
+//
+//  DriveBuddyApp.swift
+//  DriveBuddy
+//
+//  Created by Katherine Palevich on 3/9/21.
+//
+
+import SwiftUI
+
+@main
+struct DriveBuddyApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
