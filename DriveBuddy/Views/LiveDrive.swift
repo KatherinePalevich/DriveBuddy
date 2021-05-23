b3d214b09ecfdbed148a8535110c70faee34d139
KatherinePalevich <katherine.palevich@gmail.com>
2021-05-22 23:22:12 -0700

    Simplify stopwatch

--- DriveBuddy/Views/LiveDrive.swift
+++ DriveBuddy/Views/LiveDrive.swift
@@ -12,11 +12,10 @@ struct LiveDrive: View {
     @Environment(\.managedObjectContext) private var viewContext
     @ObservedObject var drive: Drive
     private let pasteboard = UIPasteboard.general
-    @ObservedObject var stopWatch = StopWatch()
     @Binding var showLiveDrive: Bool
+    @State var startDate = Date()
     @ObservedObject var locationManager = LocationManager()
     @State private var region = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 47.606, longitude: -122.332), span: MKCoordinateSpan(latitudeDelta: 5, longitudeDelta: 5))
-    
     
     var userLatitude: String {
         return "\(locationManager.lastLocation?.coordinate.latitude ?? 0)"
@@ -31,36 +30,19 @@ struct LiveDrive: View {
         NavigationView {
             ScrollView{
                 VStack {
-                    Text("Drive \(drive.wrappedDate )")
                     Text("location status: \(locationManager.statusString)")
                     HStack {
                         Text("latitude: \(userLatitude)")
                         Text("longitude: \(userLongitude)")
                     }
-                    Text("Drive Duration \(elapsedTimeFormatter(time: self.stopWatch.elapsedTime))")
-                    Text(self.stopWatch.stopWatchTime)
-                        .font(.custom("courier", size: 30))
-                        .frame(width: UIScreen.main.bounds.size.width,
-                               height: 100,
-                               alignment: .center)
-                    HStack{
-                        StopWatchButton(actions: [self.stopWatch.reset, self.stopWatch.lap],
-                                        labels: ["Reset", "Lap"],
-                                        color: Color.red,
-                                        isPaused: self.stopWatch.isPaused())
-                        
-                        StopWatchButton(actions: [self.stopWatch.start, self.stopWatch.pause],
-                                        labels: ["Start", "Pause"],
-                                        color: Color.blue,
-                                        isPaused: self.stopWatch.isPaused())
-                    }
+                    Text(startDate, style: .timer)
                     Map(coordinateRegion: $region,
                         showsUserLocation: true, userTrackingMode: .constant(.follow))
                         .frame(width: 400, height: 300)
                 }.navigationBarTitle(Text("Live Drive"), displayMode: .inline)
                 .navigationBarItems(trailing: Button(action: {
                     self.showLiveDrive = false
-                    drive.driveLength = Int32(stopWatch.elapsedTime)
+                    drive.driveLength = Int32(-startDate.timeIntervalSinceNow)
                 })  {
                     Text("Done").bold()
                 })
@@ -75,38 +57,4 @@ func elapsedTimeFormatter(time: TimeInterval) -> String {
     formatter.allowedUnits = [.second, .minute, .hour]
     
     return formatter.string(from: time)!
-}
-
-// Code borrowed from https://programmingwithswift.com/build-a-stopwatch-app-with-swiftui/
-struct StopWatchButton : View {
-    var actions: [() -> Void]
-    var labels: [String]
-    var color: Color
-    var isPaused: Bool
-    
-    var body: some View {
-        let buttonWidth = (UIScreen.main.bounds.size.width / 2) - 12
-        
-        return Button(action: {
-            if self.isPaused {
-                self.actions[0]()
-            } else {
-                self.actions[1]()
-            }
-        }) {
-            if isPaused {
-                Text(self.labels[0])
-                    .foregroundColor(Color.white)
-                    .frame(width: buttonWidth,
-                           height: 50)
-            } else {
-                Text(self.labels[1])
-                    .foregroundColor(Color.white)
-                    .frame(width: buttonWidth,
-                           height: 50)
-            }
-        }
-        .background(self.color)
-        .cornerRadius(5)
-    }
 }
