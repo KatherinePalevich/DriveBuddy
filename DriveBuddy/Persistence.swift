5940c8a3ef27f82f72687a40331af8becdbc48bc
KatherinePalevich <katherine.palevich@gmail.com>
2021-08-31 18:10:08 -0700

    Added example goals to app

--- DriveBuddy/Persistence.swift
+++ DriveBuddy/Persistence.swift
@@ -8,16 +8,37 @@
 import CoreData
 import UIKit
 
+func CreateGoal(goal: String, details: String, done: Bool, context: NSManagedObjectContext) {
+    let newGoal = Goal(context:context)
+    newGoal.goal = goal
+    newGoal.details = details
+    newGoal.done = false
+}
+
 struct PersistenceController {
     static let shared = PersistenceController()
-
+    
     static var preview: PersistenceController = {
         let result = PersistenceController(inMemory: true)
         let viewContext = result.container.viewContext
-        for _ in 0..<1 {
-            let newGoal = Goal(context: viewContext)
-            newGoal.goal = "parallel parking"
-        }
+        
+        createExampleDatabase(viewContext: viewContext)
+        return result
+    }()
+    
+    static func createExampleDatabase(viewContext: NSManagedObjectContext) {
+        CreateGoal(goal: "Leaving the Curb", details: "Signal and wait until it's safe to re-enter traffic.", done: false, context: viewContext)
+        CreateGoal(goal: "Controlling the Vehicle", details: "Use the gas pedal, brake, steering wheel, and other controls correctly. Change your speed to suit the number and speed of other vehicles, pedestrians, road conditions, weather conditions, construction, amount of light, and the distance you can see ahead.", done: false, context: viewContext)
+        CreateGoal(goal: "Switching Lanes", details: "Use Signal, Mirror, Over the Shoulder, and Go (SMOG). Switch lanes smoothly and maintain speed while keeping a safe distance from other vehicles.", done: false, context: viewContext)
+        CreateGoal(goal: "Traffic Signals", details: "Understanding traffic signals and signs.", done: false, context: viewContext)
+        CreateGoal(goal: "Intersections", details: "Scan carefully for signs, signals, pedestrians, and other vehicles. Yield and take the right-of-way correctly. Understand point of no return.", done: false, context: viewContext)
+        CreateGoal(goal: "Stopping", details: "Be able to stop smoothly and at the right spot. Be able to stop quickly and safely in an emergency.", done: false, context: viewContext)
+        CreateGoal(goal: "Backing up", details: "Look over right shoulder. Can back in a straight line. Have complete control of the vehicle.", done: false, context: viewContext)
+        CreateGoal(goal: "Parking on a Hill", details: "Turn steering wheel the correct way.", done: false, context: viewContext)
+        CreateGoal(goal: "Parallel Parking", details: "Park 12-18 inches from the curb", done: false, context: viewContext)
+        CreateGoal(goal: "Backing out of Driveway into Traffic", details: "Make the proper stops and signals, while staying close to the curb. Be alert for pedestrian traffic and other vehicles.", done: false, context: viewContext)
+        CreateGoal(goal: "Arm Signals", details: "Arms signals for right/left turns and slow or stop.", done: false, context: viewContext)
+        
         do {
             try viewContext.save()
         } catch {
@@ -26,31 +47,24 @@ struct PersistenceController {
             let nsError = error as NSError
             fatalError("Unresolved error \(nsError), \(nsError.userInfo)")
         }
-        return result
-    }()
-
+    }
+    
     let container: NSPersistentContainer
-
+    
     init(inMemory: Bool = false) {
         container = NSPersistentContainer(name: "DriveBuddy")
         if inMemory {
             container.persistentStoreDescriptions.first!.url = URL(fileURLWithPath: "/dev/null")
         }
-        container.loadPersistentStores(completionHandler: { (storeDescription, error) in
+        container.loadPersistentStores(completionHandler: { [self] (storeDescription, error) in
             if let error = error as NSError? {
-                // Replace this implementation with code to handle the error appropriately.
-                // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
-
-                /*
-                Typical reasons for an error here include:
-                * The parent directory does not exist, cannot be created, or disallows writing.
-                * The persistent store is not accessible, due to permissions or data protection when the device is locked.
-                * The device is out of space.
-                * The store could not be migrated to the current model version.
-                Check the error message to determine what the actual problem was.
-                */
                 fatalError("Unresolved error \(error), \(error.userInfo)")
-            }
+            } else {
+                let viewContext = self.container.viewContext
+                if inMemory || (try? viewContext.count(for: Goal.fetchRequest())) == 0 {
+                  PersistenceController.createExampleDatabase(viewContext:viewContext)
+                }
+              }
         })
         
         // Automatically persist when entering background.
